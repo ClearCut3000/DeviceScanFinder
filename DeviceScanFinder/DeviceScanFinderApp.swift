c2e73b338095634b18e1ea3e667675f410214146
NIKOLAY NIKITIN <nikitin.nikolay.v@gmail.com>
2023-09-12 12:35:24 +0300

    Initial Commit

---
+++ DeviceScanFinder/DeviceScanFinderApp.swift
@@ -0,0 +1,17 @@
+//
+//  DeviceScanFinderApp.swift
+//  DeviceScanFinder
+//
+//  Created by Николай Никитин on 12.09.2023.
+//
+
+import SwiftUI
+
+@main
+struct DeviceScanFinderApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
